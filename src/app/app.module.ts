d6ebdedfe19c46eacc16bcc51f6527047bc059f6
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-02-08 07:17:30 +0100

    mise en place du formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { LoginComponent } from './admin/login/login.component';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { OrderComponent } from './admin/order/order.component';
+import { CustomerComponent } from './admin/customer/customer.component';
+import { CartComponent } from './admin/cart/cart.component';
+import { CategoriesComponent } from './admin/categories/categories.component';
+import { LandingComponent } from './website/landing/landing.component';
+import { CategoryProductsComponent } from './website/category-products/category-products.component';
+import { CustomerCartComponent } from './website/customer-cart/customer-cart.component';
+import { CheckoutComponent } from './website/checkout/checkout.component';
+import { CustomerOrdersComponent } from './website/customer-orders/customer-orders.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    LoginComponent,
+    LayoutComponent,
+    OrderComponent,
+    CustomerComponent,
+    CartComponent,
+    CategoriesComponent,
+    LandingComponent,
+    CategoryProductsComponent,
+    CustomerCartComponent,
+    CheckoutComponent,
+    CustomerOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
