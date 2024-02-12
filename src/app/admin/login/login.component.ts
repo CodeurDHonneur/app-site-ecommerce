986d1ed0d0b80fba96f4a2a1443d94db4ee6870d
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-02-12 23:05:39 +0100

    composant product

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   loginObj: any = {
     userName: '',
     password: ''
   }
+
+  constructor(private router: Router){
+   
+  }
+
+  onLogin(){
+    if(this.loginObj.userName == "Admin" && this.loginObj.password == "azerty123"){
+      this.router.navigateByUrl('/products')
+    } else {
+     alert('Désolé, vous ne pouvez pas vous connecter')
+    }
+   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
 import { LoginComponent } from './admin/login/login.component';
+import { ProductsComponent } from './admin/products/products.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,17 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'products',
+        component: ProductsComponent
+      }
+
+    ]
   }
 ];
 
